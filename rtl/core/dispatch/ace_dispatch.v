f598f938bb53b8d5fbdbfa5df3f9285714ccdc26
ejune <ejune@aureage.com>
2017-10-17 22:22:04 +0800

    add dispatch stage file

---
+++ rtl/core/dispatch/ace_dispatch.v
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name   : ace_dispatch.v
+//  Author      : ejune@aureage.com
+//                
+//  Description : dispatch module dispatches renamed packets to Issue Queue, Active
+//                List, and Load-Store queue.
+//                Before dipatching it checks if there is enough space for incoming
+//                instructions in Issue Queue, Active List, and Load-Store queue.
+//                Dispatch width is same as rename width 
+//                
+//                
+//  Create Date : original_time
+//  Version     : v0.1 
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+module ace_dispatch ();
+
+
+endmodule
